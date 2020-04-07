b875f66cbabfb565ab167b7bde9a4d1ce59996d0
Omar Portillo <OmrPrt@aol.com>
2020-04-07 19:08:48 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent:'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Course Goal" 
+        style={{width: 200, borderColor: 'black', borderWidth: 1, padding: 10}}
+         />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
